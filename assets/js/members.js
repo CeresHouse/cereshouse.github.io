11b5854d491b3eb661761a19eea0935f0e8e1ad5
Zenerboson <zenerboson@gmail.com>
2017-04-26 12:49:25 -0700

    Initial commit

---
+++ assets/js/members.js
@@ -0,0 +1,363 @@
+var members = [
+{
+  firstname: "Shannon",
+  year: "2020"
+},
+{
+  firstname: "Cevi",
+  year: "2020"
+},
+{
+  firstname: "Max",
+  year: "2020"
+},
+{
+  firstname: "Megan",
+  year: "2020"
+},
+{
+  firstname: "Jeremy",
+  year: "2020"
+},
+{
+  firstname: "Grace",
+  year: "2020"
+},
+{
+  firstname: "Finley",
+  year: "2020"
+},
+{
+  firstname: "Molly",
+  year: "2020"
+},
+{
+  firstname: "Christopher",
+  year: "2020"
+},
+{
+  firstname: "Nikhil",
+  year: "2020"
+},
+{
+  firstname: "Tyler",
+  year: "2020"
+},
+{
+  firstname: "Izzy",
+  year: "2020"
+},
+{
+  firstname: "Rachel",
+  year: "2020"
+},
+{
+  firstname: "Quincy",
+  year: "2020"
+},
+{
+  firstname: "Clay",
+  year: "2020"
+},
+{
+  firstname: "Wesley",
+  year: "2020"
+},
+{
+  firstname: "Sophia",
+  year: "2020"
+},
+{
+  firstname: "Laura",
+  year: "2020"
+},
+{
+  firstname: "Cory",
+  year: "2020"
+},
+{
+  firstname: "Sanjana",
+  year: "2020"
+},
+{
+  firstname: "Stanley",
+  year: "2020"
+},
+{
+  firstname: "Gregoire",
+  year: "2019"
+},
+{
+  firstname: "Audrey",
+  year: "2019"
+},
+{
+  firstname: "Caleb",
+  year: "2019"
+},
+{
+  firstname: "Alexandra",
+  year: "2019"
+},
+{
+  firstname: "Ethan",
+  year: "2019"
+},
+{
+  firstname: "Kyle",
+  year: "2019"
+},
+{
+  firstname: "Gitika",
+  year: "2019"
+},
+{
+  firstname: "Grace",
+  year: "2019"
+},
+{
+  firstname: "Robert",
+  year: "2019"
+},
+{
+  firstname: "Sanam",
+  year: "2019"
+},
+{
+  firstname: "Mickey",
+  year: "2019"
+},
+{
+  firstname: "Eton",
+  year: "2019"
+},
+{
+  firstname: "Kayla",
+  year: "2019"
+},
+{
+  firstname: "Audrey",
+  year: "2019"
+},
+{
+  firstname: "Shaheen",
+  year: "2019"
+},
+{
+  firstname: "Ben",
+  year: "2019"
+},
+{
+  firstname: "Liam",
+  year: "2019"
+},
+{
+  firstname: "Tom",
+  year: "2019"
+},
+{
+  firstname: "Ethan",
+  year: "2018"
+},
+{
+  firstname: "Megan",
+  year: "2018"
+},
+{
+  firstname: "Ishani",
+  year: "2018"
+},
+{
+  firstname: "Quantum",
+  year: "2018"
+},
+{
+  firstname: "Justin",
+  year: "2018"
+},
+{
+  firstname: "Isha",
+  year: "2018"
+},
+{
+  firstname: "Leo",
+  year: "2018"
+},
+{
+  firstname: "Julia",
+  year: "2018"
+},
+{
+  firstname: "Joseph",
+  year: "2018"
+},
+{
+  firstname: "Elaine",
+  year: "2018"
+},
+{
+  firstname: "Hilary",
+  year: "2018"
+},
+{
+  firstname: "Jessa",
+  year: "2018"
+},
+{
+  firstname: "AJ",
+  year: "2018"
+},
+{
+  firstname: "Michael",
+  year: "2018"
+},
+{
+  firstname: "Emma",
+  year: "2018"
+},
+{
+  firstname: "Julie",
+  year: "2018"
+},
+{
+  firstname: "Neeraj",
+  year: "2018"
+},
+{
+  firstname: "Ricardo",
+  year: "2018"
+},
+{
+  firstname: "Kathryn",
+  year: "2017"
+},
+{
+  firstname: "Nathan",
+  year: "2017"
+},
+{
+  firstname: "George",
+  year: "2017"
+},
+{
+  firstname: "Gail",
+  year: "2017"
+},
+{
+  firstname: "Juliana",
+  year: "2017"
+},
+{
+  firstname: "Ethan",
+  year: "2017"
+},
+{
+  firstname: "Jackie",
+  year: "2017"
+},
+{
+  firstname: "Martin",
+  year: "2017"
+},
+{
+  firstname: "Carter",
+  year: "2017"
+},
+{
+  firstname: "Jill",
+  year: "2017"
+},
+{
+  firstname: "Christine",
+  year: "2017"
+},
+{
+  firstname: "Nolan",
+  year: "2017"
+},
+{
+  firstname: "Matthew",
+  year: "2017"
+},
+{
+  firstname: "Olivia",
+  year: "2017"
+},
+{
+  firstname: "Alex",
+  year: "2017"
+},
+{
+  firstname: "Saira",
+  year: "2017"
+},
+{
+  firstname: "Claire",
+  year: "Instructor"
+},
+{
+  firstname: "Jan",
+  year: "Instructor"
+},
+{
+  firstname: "Lee",
+  year: "Instructor"
+},
+{
+  firstname: "Rob",
+  year: "Instructor"
+},
+{
+  firstname: "Cole",
+  year: "Instructor"
+},
+{
+  firstname: "Claire",
+  year: "Instructor"
+},
+{
+  firstname: "Jo",
+  year: "Instructor"
+},
+{
+  firstname: "Cole",
+  year: "Instructor"
+},
+{
+  firstname: "Ali",
+  year: "Instructor"
+},
+{
+  firstname: "Brian",
+  year: "Instructor"
+},
+{
+  firstname: "Grant",
+  year: "Instructor"
+},
+{
+  firstname: "Danielle",
+  year: "Instructor"
+},
+{
+  firstname: "Juliet",
+  year: "Instructor"
+},
+{
+  firstname: "Luke",
+  year: "Instructor"
+},
+{
+  firstname: "Cole",
+  year: "Instructor"
+},
+{
+  firstname: "Veronique",
+  year: "Instructor"
+},
+{
+  firstname: "Stephanie",
+  year: "Instructor"
+},
+]
+members = members.reverse()
