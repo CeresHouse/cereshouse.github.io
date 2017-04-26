17c88cb8fc6f8597903b5912650dcf1836d18e0b
Zenerboson <zenerboson@gmail.com>
2017-04-26 14:47:48 -0700

    added last initials

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,362 +1,452 @@
 var members = [
 {
   firstname: "Shannon",
+  lastname: "C",
   year: "2020"
 },
 {
   firstname: "Cevi",
+  lastname: "B",
   year: "2020"
 },
 {
   firstname: "Max",
+  lastname: "G",
   year: "2020"
 },
 {
   firstname: "Megan",
+  lastname: "F",
   year: "2020"
 },
 {
   firstname: "Jeremy",
+  lastname: "D",
   year: "2020"
 },
 {
   firstname: "Grace",
+  lastname: "L",
   year: "2020"
 },
 {
   firstname: "Finley",
+  lastname: "F",
   year: "2020"
 },
 {
   firstname: "Molly",
+  lastname: "K",
   year: "2020"
 },
 {
   firstname: "Christopher",
+  lastname: "M",
   year: "2020"
 },
 {
   firstname: "Nikhil",
+  lastname: "P",
   year: "2020"
 },
 {
   firstname: "Tyler",
+  lastname: "G",
   year: "2020"
 },
 {
   firstname: "Izzy",
+  lastname: "S",
   year: "2020"
 },
 {
   firstname: "Rachel",
+  lastname: "D",
   year: "2020"
 },
 {
   firstname: "Quincy",
+  lastname: "A",
   year: "2020"
 },
 {
   firstname: "Clay",
+  lastname: "A",
   year: "2020"
 },
 {
   firstname: "Wesley",
+  lastname: "S",
   year: "2020"
 },
 {
   firstname: "Sophia",
+  lastname: "S",
   year: "2020"
 },
 {
   firstname: "Laura",
+  lastname: "S",
   year: "2020"
 },
 {
   firstname: "Cory",
+  lastname: "T",
   year: "2020"
 },
 {
   firstname: "Sanjana",
+  lastname: "V",
   year: "2020"
 },
 {
   firstname: "Stanley",
+  lastname: "W",
   year: "2020"
 },
 {
   firstname: "Gregoire",
+  lastname: "B",
   year: "2019"
 },
 {
   firstname: "Audrey",
+  lastname: "C",
   year: "2019"
 },
 {
   firstname: "Caleb",
+  lastname: "D",
   year: "2019"
 },
 {
   firstname: "Alexandra",
+  lastname: "H",
   year: "2019"
 },
 {
   firstname: "Ethan",
+  lastname: "K",
   year: "2019"
 },
 {
   firstname: "Kyle",
+  lastname: "M",
   year: "2019"
 },
 {
   firstname: "Gitika",
+  lastname: "P",
   year: "2019"
 },
 {
   firstname: "Grace",
+  lastname: "W",
   year: "2019"
 },
 {
   firstname: "Robert",
+  lastname: "V",
   year: "2019"
 },
 {
   firstname: "Sanam",
+  lastname: "Y",
   year: "2019"
 },
 {
   firstname: "Mickey",
+  lastname: "D",
   year: "2019"
 },
 {
   firstname: "Eton",
+  lastname: "S",
   year: "2019"
 },
 {
   firstname: "Kayla",
+  lastname: "W",
   year: "2019"
 },
 {
   firstname: "Audrey",
+  lastname: "C",
   year: "2019"
 },
 {
   firstname: "Shaheen",
+  lastname: "C",
   year: "2019"
 },
 {
   firstname: "Ben",
+  lastname: "S",
   year: "2019"
 },
 {
   firstname: "Liam",
+  lastname: "M",
   year: "2019"
 },
 {
   firstname: "Tom",
+  lastname: "S",
   year: "2019"
 },
 {
   firstname: "Ethan",
+  lastname: "L",
   year: "2018"
 },
 {
   firstname: "Megan",
+  lastname: "W",
   year: "2018"
 },
 {
   firstname: "Ishani",
+  lastname: "G",
   year: "2018"
 },
 {
   firstname: "Quantum",
+  lastname: "Z",
   year: "2018"
 },
 {
   firstname: "Justin",
+  lastname: "S",
   year: "2018"
 },
 {
   firstname: "Isha",
+  lastname: "R",
   year: "2018"
 },
 {
   firstname: "Leo",
+  lastname: "R",
   year: "2018"
 },
 {
   firstname: "Julia",
+  lastname: "R",
   year: "2018"
 },
 {
   firstname: "Joseph",
+  lastname: "Q",
   year: "2018"
 },
 {
   firstname: "Elaine",
+  lastname: "W",
   year: "2018"
 },
 {
   firstname: "Hilary",
+  lastname: "N",
   year: "2018"
 },
 {
   firstname: "Jessa",
+  lastname: "M",
   year: "2018"
 },
 {
   firstname: "AJ",
+  lastname: "N",
   year: "2018"
 },
 {
   firstname: "Michael",
+  lastname: "B",
   year: "2018"
 },
 {
   firstname: "Emma",
+  lastname: "D",
   year: "2018"
 },
 {
   firstname: "Julie",
+  lastname: "G",
   year: "2018"
 },
 {
   firstname: "Neeraj",
+  lastname: "S",
   year: "2018"
 },
 {
   firstname: "Ricardo",
+  lastname: "G",
   year: "2018"
 },
 {
   firstname: "Kathryn",
+  lastname: "S",
   year: "2017"
 },
 {
   firstname: "Nathan",
+  lastname: "K",
   year: "2017"
 },
 {
   firstname: "George",
+  lastname: "H",
   year: "2017"
 },
 {
   firstname: "Gail",
+  lastname: "R",
   year: "2017"
 },
 {
   firstname: "Juliana",
+  lastname: "K",
   year: "2017"
 },
 {
   firstname: "Ethan",
+  lastname: "L",
   year: "2017"
 },
 {
   firstname: "Jackie",
+  lastname: "M",
   year: "2017"
 },
 {
   firstname: "Martin",
+  lastname: "R",
   year: "2017"
 },
 {
   firstname: "Carter",
+  lastname: "T",
   year: "2017"
 },
 {
   firstname: "Jill",
+  lastname: "M",
   year: "2017"
 },
 {
   firstname: "Christine",
+  lastname: "C",
   year: "2017"
 },
 {
   firstname: "Nolan",
+  lastname: "M",
   year: "2017"
 },
 {
   firstname: "Matthew",
+  lastname: "M",
   year: "2017"
 },
 {
   firstname: "Olivia",
+  lastname: "J",
   year: "2017"
 },
 {
   firstname: "Alex",
+  lastname: "C",
   year: "2017"
 },
 {
   firstname: "Saira",
+  lastname: "Y",
   year: "2017"
 },
 {
   firstname: "Claire",
+  lastname: "L",
   year: "Instructor"
 },
 {
   firstname: "Jan",
+  lastname: "P",
   year: "Instructor"
 },
 {
   firstname: "Lee",
+  lastname: "H",
   year: "Instructor"
 },
 {
   firstname: "Rob",
+  lastname: "Z",
   year: "Instructor"
 },
 {
   firstname: "Cole",
+  lastname: "G",
   year: "Instructor"
 },
 {
   firstname: "Claire",
+  lastname: "Y",
   year: "Instructor"
 },
 {
   firstname: "Jo",
+  lastname: "N",
   year: "Instructor"
 },
 {
   firstname: "Cole",
+  lastname: "G",
   year: "Instructor"
 },
 {
   firstname: "Ali",
+  lastname: "M",
   year: "Instructor"
 },
 {
   firstname: "Brian",
+  lastname: "C",
   year: "Instructor"
 },
 {
   firstname: "Grant",
+  lastname: "A",
   year: "Instructor"
 },
 {
   firstname: "Danielle",
+  lastname: "D",
   year: "Instructor"
 },
 {
   firstname: "Juliet",
+  lastname: "T",
   year: "Instructor"
 },
 {
   firstname: "Luke",
+  lastname: "D",
   year: "Instructor"
 },
 {
   firstname: "Cole",
+  lastname: "G",
   year: "Instructor"
 },
 {
   firstname: "Veronique",
+  lastname: "M",
   year: "Instructor"
 },
 {
   firstname: "Stephanie",
+  lastname: "S",
   year: "Instructor"
 },
 ]
